92cf243d8bf3671137dace7d1d099b3edac4731e
Daniel Bornstrand <daniel.bornstrand@gloot.com>
2019-05-05 15:54:22 +0200

    Added socket server

--- src/containers/contenders/index.jsx
+++ src/containers/contenders/index.jsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import io from 'socket.io-client';
 
 export default function Contenders() {
+  useEffect(() => {
+    const socket = io('http://localhost:3000');
+  }, []);
   return <div>Contenders</div>;
 }
